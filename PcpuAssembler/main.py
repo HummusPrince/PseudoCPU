975f32c92bf91328813fb4e0624d19859cccc405
nadavnave <nave.nadav@gmail.com>
2022-09-03 18:34:40 +0300

    finished second pass

--- PcpuAssembler/main.py
+++ PcpuAssembler/main.py
@@ -1,32 +1,116 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='PcpuAsslembler is simple two pass assembler for PseudoCpu educational exerice')
-parser.add_argument('input_file', metavar='input_file', type=str, help="input .asm file to assemble")
+parser.add_argument('input_file', metavar='input_file', type=str, help="input .asm path to assemble")
+parser.add_argument('output_file', metavar='output_file', type=str, help="output .bin path ")
 commands = [ 'JMP', 'JEQ', 'JGE', 'JBZ', 'SUB', 'SHF']
 registers = ['A', 'B']
+
+def construct_command(so=0, sw=0, wb=0, wa=0, jc=0, ju=0, ad=0):
+    line = "{}{}_{}{}{}_{}{}_{}{}{}{}".format((so>>1)&1, so&1, sw&1, wb&1, wa&1,jc&1, ju&1, (ad>>3)&1, (ad>>2)&1, (ad>>1)&1, ad&1)
+    return line
+    
+EQ      = 0
+GE      = 1
+BZ      = 2
+
+SHF     = 0
+SUB     = 1
+
+def parse_command(line, labels):
+    command = line[0:3]
+    com_arg = [x.strip() for x in line[3:].split(",")]
+
+    if command not in commands:
+        print("Error! command {} in not valid".format(command))
+        return
+
+    if command == 'JMP':
+        label = com_arg[0]
+        command_bin = construct_command(ju=1, ad=labels[label])
+
+    elif command == 'JEQ':
+        label = com_arg[-1]
+        command_bin = construct_command(so=EQ, jc=1, ad=labels[label])
+
+    elif command == 'JGE':
+        label = com_arg[-1]
+        if com_arg[0] == 'A':
+            command_bin = construct_command(so=GE, jc=1, ad=labels[label])
+        else:
+            command_bin = construct_command(sw=1, so=GE, jc=1, ad=labels[label])
+
+    elif command == 'JBZ':
+        label = com_arg[-1]
+        if com_arg[0] == 'A':
+            command_bin = construct_command(so=BZ, jc=1, ad=labels[label])
+        else:
+            command_bin = construct_command(sw=1, so=BZ, jc=1, ad=labels[label])
+
+    elif command == 'SUB':
+        if com_arg[0] == 'A':
+            command_bin = construct_command(so=SUB, wa=1)
+        else:
+            command_bin = construct_command(sw=1, so=SUB, wb=1)
+            
+    else :  # command == 'SHF':
+        if com_arg[0] == 'A':
+            command_bin = construct_command(so=SHF, wa=1)
+        else:
+            command_bin = construct_command(sw=1, so=SHF, wb=1)
+
+    return command_bin
+
+
+def secondpass(input_file, labels):
+    with open(input_file,"r") as f:
+        bin_lines = []
+        for line in f.readlines():
+            line = line.split(';')[0].strip()
+            if (line == ""):        # empty line
+                continue
+            elif ( ':' in line):    # label line
+                continue
+            else :                  # command line
+                bin_lines.append(parse_command(line, labels))
+
+    return "\n".join(bin_lines)
+
+
+
         
 def firstpass(input_file):
     with open(input_file,"r") as f:
         address = 0
         labels = {} 
         for line in f.readlines():
-            print(line)
             line = line.split(';')[0].strip()       # ignore comments
-            print(line)
-            if (line == ""):
+            if (line == ""):    # empty line
                 continue
-            elif ':' in line:
+            elif ':' in line:   # label line
                 label = line.split(':')[0]
                 labels[label] = address
-            else :
+            else :              # command line
                 address += 1
 
     return labels
         
 def PcpuAssmbler_main():
     args = parser.parse_args()
+    print("Starting first pass")
     labels = firstpass(args.input_file)
-    print(labels)
+    print("Found {} labels".format(len(labels)))
+    print("Starting second pass")
+    binfile = secondpass(args.input_file, labels)
+    print("Generated bin file:")
+    print("----------------------------")
+    print(binfile)
+    print("----------------------------\n")
+    print("writing bin file to {}".format(args.output_file))
+    with open(args.output_file, "w") as f:
+        f.write(binfile)
+    print("Finished")
+
 
 
 
